461cdc3a57a1f40966bce89db7598b04b41f05e6
Joel Pascual <joeltrabajo92@gmail.com>
2022-12-02 12:20:30 +0100

    Changes to be committed: 	new file:   LucaTickets-User/doc/allclasses-index.html 	new file:   LucaTickets-User/doc/allpackages-index.html 	new file:   LucaTickets-User/doc/com/proyecto/spring/user/LucaTicketsUserApplication.html 	new file:   LucaTickets-User/doc/com/proyecto/spring/user/class-use/LucaTicketsUserApplication.html 	new file:   LucaTickets-User/doc/com/proyecto/spring/user/model/User.html 	new file:   LucaTickets-User/doc/com/proyecto/spring/user/model/class-use/User.html 	new file:   LucaTickets-User/doc/com/proyecto/spring/user/model/package-summary.html 	new file:   LucaTickets-User/doc/com/proyecto/spring/user/model/package-tree.html 	new file:   LucaTickets-User/doc/com/proyecto/spring/user/model/package-use.html 	new file:   LucaTickets-User/doc/com/proyecto/spring/user/package-summary.html 	new file:   LucaTickets-User/doc/com/proyecto/spring/user/package-tree.html 	new file:   LucaTickets-User/doc/com/proyecto/spring/user/package-use.html 	new file:   LucaTickets-User/doc/element-list 	new file:   LucaTickets-User/doc/help-doc.html 	new file:   LucaTickets-User/doc/index-files/index-1.html 	new file:   LucaTickets-User/doc/index-files/index-2.html 	new file:   LucaTickets-User/doc/index-files/index-3.html 	new file:   LucaTickets-User/doc/index-files/index-4.html 	new file:   LucaTickets-User/doc/index.html 	new file:   LucaTickets-User/doc/jquery-ui.overrides.css 	new file:   LucaTickets-User/doc/legal/ADDITIONAL_LICENSE_INFO 	new file:   LucaTickets-User/doc/legal/ASSEMBLY_EXCEPTION 	new file:   LucaTickets-User/doc/legal/LICENSE     new file:   LucaTickets-User/doc/legal/jquery.md 	new file:   LucaTickets-User/doc/legal/jqueryUI.md 	new file:   LucaTickets-User/doc/member-search-index.js 	new file:   LucaTickets-User/doc/module-search-index.js 	new file:   LucaTickets-User/doc/overview-summary.html 	new file:   LucaTickets-User/doc/overview-tree.html 	new file:   LucaTickets-User/doc/package-search-index.js 	new file:   LucaTickets-User/doc/resources/glass.png 	new file:   LucaTickets-User/doc/resources/x.png 	new file:   LucaTickets-User/doc/script-dir/images/ui-bg_glass_55_fbf9ee_1x400.png 	new file:   LucaTickets-User/doc/script-dir/images/ui-bg_glass_65_dadada_1x400.png 	new file:   LucaTickets-User/doc/script-dir/images/ui-bg_glass_75_dadada_1x400.png 	new file:   LucaTickets-User/doc/script-dir/images/ui-bg_glass_75_e6e6e6_1x400.png 	new file:   LucaTickets-User/doc/script-dir/images/ui-bg_glass_95_fef1ec_1x400.png 	new file:   LucaTickets-User/doc/script-dir/images/ui-bg_highlight-soft_75_cccccc_1x100.png 	new file:   LucaTickets-User/doc/script-dir/images/ui-icons_222222_256x240.png 	new file:   LucaTickets-User/doc/script-dir/images/ui-icons_2e83ff_256x240.png 	new file:   LucaTickets-User/doc/script-dir/images/ui-icons_454545_256x240.png 	new file:   LucaTickets-User/doc/script-dir/images/ui-icons_888888_256x240.png 	new file:   LucaTickets-User/doc/script-dir/images/ui-icons_cd0a0a_256x240.png 	new file:   LucaTickets-User/doc/script-dir/jquery-3.5.1.min.js 	new file:   LucaTickets-User/doc/script-dir/jquery-ui.min.css 	new file:   LucaTickets-User/doc/script-dir/jquery-ui.min.js 	new file:   LucaTickets-User/doc/script-dir/jquery-ui.structure.min.css 	new file:   LucaTickets-User/doc/script.js 	new file:   LucaTickets-User/doc/search.js 	new file:   LucaTickets-User/doc/stylesheet.css 	new file:   LucaTickets-User/doc/tag-search-index.js 	new file:   LucaTickets-User/doc/type-search-index.js 	modified:   LucaTickets-User/src/main/java/com/proyecto/spring/user/model/User.java
    Javadoc añadido al proyecto, falta por pulir

--- LucaTickets-User/src/main/java/com/proyecto/spring/user/model/User.java
+++ LucaTickets-User/src/main/java/com/proyecto/spring/user/model/User.java
@@ -9,20 +9,62 @@ import javax.persistence.Id;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+/**
+ * 
+ * @author Joel Pascual Mahindo
+ * @version 0.1
+ * @since 0.1
+ * 
+ * */
 
 @NoArgsConstructor
 @Entity
 @Data
 public class User {
 	
+	
+	/**
+	 * 
+	 * @param id: Campo identificativo del resgistro
+	 * 
+	 * */
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	/**
+	 * 
+	 * @param nombre: Como se llama el usuario
+	 * 
+	 * */
 	private String nombre;	
+	
+	/**
+	 * 
+	 * @param apellido: Como se apellida el usuario
+	 * 
+	 * */
 	private String apellido;
+	
+	/**
+	 * 
+	 * @param amail: Correo electrónico del usuario
+	 * 
+	 * */
 	private String mail;
-	private String contraseña;
+	
+	/**
+	 * 
+	 * @param contraseña: Contraseña de la cuenta
+	 * 
+	 * */
+	private String contrasenya;
+	
+	/**
+	 * 
+	 * @param Fecha de alta: Cuando se dió de alta en la aplicación
+	 * 
+	 * */
 	private Date fecha_alta;
 	
 	
