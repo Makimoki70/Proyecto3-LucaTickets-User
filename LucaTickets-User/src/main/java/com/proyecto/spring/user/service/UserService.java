1b2c8c69e2006f432aefd1dab88cf4ca75b548be
Alberto <makimoki69@gmail.com>
2022-12-02 14:04:28 +0100

    Controllador añadido y post realizado

--- LucaTickets-User/src/main/java/com/proyecto/spring/user/service/UserService.java
+++ LucaTickets-User/src/main/java/com/proyecto/spring/user/service/UserService.java
@@ -3,26 +3,21 @@ package com.proyecto.spring.user.service;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.client.RestTemplate;
+//import org.springframework.web.client.RestTemplate;
+import org.springframework.stereotype.Service;
 
 import com.proyecto.spring.user.model.User;
 import com.proyecto.spring.user.repository.DAOUser;
 
+@Service
 public class UserService {
 	
 	@Autowired
 	private DAOUser daoUser;
 
-	public void addUser(Long id, String nombre, String apellido, String mail, String contrasena, Date fecha_alta) {
-			final User user = daoUser.findById(id).orElseThrow();
+	public User addUser(User user) {
+		//final User userdb = daoUser.findById(id).orElseThrow();
 		
-			//Añadir los setters
-			User user = new User();
-			user.setNombre(nombre);
-			user.setApellido(apellido);
-			user.setMail(mail);
-			user.setContrasena(contrasena);
-			user.setDate(fecha_alta);
-			daoUser.save(user);
+		return daoUser.save(user);
 	}
 }
