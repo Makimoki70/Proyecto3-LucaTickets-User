2acc21c11d42f2c444a3fbd34856c5c6e9b0e1a0
David Gómez <azhu@hotmail.es>
2022-12-05 14:07:40 +0100

    Añadido de OpenAPI, Añadido de JavaDoc, Añadidos Test (No funcionales aun)

--- LucaTickets-User/src/main/java/com/proyecto/spring/user/model/User.java
+++ LucaTickets-User/src/main/java/com/proyecto/spring/user/model/User.java
@@ -6,13 +6,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
-import com.proyecto.spring.user.model.response.DTOUser;
-
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 /**
  * 
+ * Nombre de clase: User.
+ * Descripcion: Clase model del microservicio User. 
+ * Fecha: 05/12/2022
  * @author Joel Pascual Mahindo
  * @version 0.1
  * @since 0.1
@@ -22,6 +25,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Data
+@Schema(name="User", description = "User Class")
 public class User {
 	
 	/**
@@ -31,6 +35,10 @@ public class User {
 	 * */
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@Schema(name= "id", 
+	description = "Identificador único para el usuario", 
+    example = "42", 
+    required = true)
 	private Long id;
 	
 	/**
@@ -38,6 +46,7 @@ public class User {
 	 * @param nombre: Como se llama el usuario
 	 * 
 	 * */
+	@Size(min = 0, max = 45)
 	private String nombre;	
 	
 	/**
@@ -45,6 +54,7 @@ public class User {
 	 * @param apellido: Como se apellida el usuario
 	 * 
 	 * */
+	@Size(min = 0, max = 45)
 	private String apellido;
 	
 	/**
@@ -52,13 +62,15 @@ public class User {
 	 * @param amail: Correo electrónico del usuario
 	 * 
 	 * */
+	@Size(min = 0, max = 45)
 	private String mail;
 	
 	/**
 	 * 
-	 * @param contraseña: Contraseña de la cuenta
+	 * @param pass: Contraseña de la cuenta
 	 * 
 	 * */
+	@Size(min = 0, max = 45)
 	private String pass;
 	
 	/**
---
+++ LucaTickets-User/src/test/java/com/proyecto/spring/user/Test02_DatabaseVacia.java
@@ -0,0 +1,28 @@
+package com.proyecto.spring.user;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+import com.proyecto.spring.user.model.User;
+
+@SpringBootTest
+public class Test02_DatabaseVacia {
+	@LocalServerPort
+    private int port;
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@Test
+	void contextLoads() {
+		ResponseEntity<User[]> response = restTemplate.getForEntity("http://localhost:" + port + "/user", User[].class);
+	    User[] users = response.getBody();
+		assertFalse(users.length == 0);
+	}
+}
